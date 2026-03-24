cf8a53eaaca006f6d87b63e19271ea5299c1b711
Stanislav N Mikhailov <stas@enso.pro>
2026-03-24 21:52:12 +0300

    Replace monolithic renderer with aggregate header

--- include/display/renderer.h
+++ include/display/renderer.h
@@ -1,63 +1,7 @@
 #pragma once
 
-#include <stddef.h>
-#include <stdint.h>
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#define RGB565(R, G, B) \
-    ((uint16_t)((((uint16_t)(R) & 0xF8u) << 8) | \
-                (((uint16_t)(G) & 0xFCu) << 3) | \
-                (((uint16_t)(B) & 0xF8u) >> 3)))
-
-#define BSWAP16(X) \
-    ((uint16_t)((((uint16_t)(X) & 0x00FFu) << 8) | \
-                (((uint16_t)(X) & 0xFF00u) >> 8)))
-
-/* RGB888 -> RGB565 with swapped byte order for ctx->buf */
-#define RGB16(R, G, B) BSWAP16(RGB565((R), (G), (B)))
-
-typedef struct
-{
-    uint16_t* buf;
-    uint16_t width;
-    uint16_t height;
-    uint16_t clip_x0;
-    uint16_t clip_y0;
-    uint16_t clip_x1;
-    uint16_t clip_y1;
-} render_ctx_t;
-
-void render_begin(render_ctx_t* ctx, uint16_t* buf, uint16_t width, uint16_t height);
-void render_set_clip(render_ctx_t* ctx, uint16_t x, uint16_t y, uint16_t width, uint16_t height);
-void render_reset_clip(render_ctx_t* ctx);
-
-void render_clear(render_ctx_t* ctx, uint16_t color);
-void render_pixel(render_ctx_t* ctx, int x, int y, uint16_t color);
-void render_line(render_ctx_t* ctx, int x0, int y0, int x1, int y1, uint16_t color);
-void render_grid(render_ctx_t* ctx, uint16_t x, uint16_t y, uint16_t step, uint16_t color);
-
-void render_sine_wave(
-    render_ctx_t* ctx,
-    uint16_t num_points,
-    int amplitude,
-    float frequency,
-    int offset_x,
-    int offset_y,
-    float phase_shift,
-    uint16_t color
-);
-
-void render_bezier(
-    render_ctx_t* ctx,
-    const int* points_x,
-    const int* points_y,
-    size_t num_points,
-    uint16_t color
-);
-
-#ifdef __cplusplus
-}
-#endif
+#include "display/render/context.h"
+#include "display/render/line.h"
+#include "display/render/grid.h"
+#include "display/render/sine_wave.h"
+#include "display/render/bezier.h"
