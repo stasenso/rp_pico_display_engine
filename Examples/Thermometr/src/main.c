90c0d9d30aa5305b7ed0f4ebf75656f052d2e3a2
Stanislav N Mikhailov <stas@enso.pro>
2026-03-25 21:35:28 +0300

    Add font rendering checks to Thermometr example

--- Examples/Thermometr/src/main.c
+++ Examples/Thermometr/src/main.c
@@ -3,6 +3,7 @@
 #include "display/render/context.h"
 #include "display/render/grid.h"
 #include "display/render/sine_wave.h"
+#include "Font/font_data.h"
 
 
 #define WIDTH   320
@@ -14,7 +15,6 @@ static void on_frame_done(void)
     // В режиме SAFE смена буферов выполняется внутри submit()
     display_submit();
 }
-
 
 int main()
 {
@@ -46,7 +46,9 @@ int main()
         render_clear(&rc, RGB16(9,19,9));
         render_grid(&rc, 20, 20, 40, RGB16(12,26,13));
         render_sine_wave(&rc, WIDTH, 50, 2.0f, 0, HEIGHT / 2, phase, RGB16(0,255,0));
-
+        draw_string(&rc, 50, 90, L"Проверка кириллицы", RGB565(255, 255, 255));
+        draw_string(&rc, 45, 110, L"Proverka latinyanskogo", RGB565(0, 0, 255));
+        draw_string(&rc, 35, 130, L"1234567890!@#$%%^&*()", RGB565(255, 0, 0));
         phase += 0.08f;
         if (phase > 6.2831853f)
         {
